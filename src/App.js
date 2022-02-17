01557600e428e4e383904d12e2b54b65f37189f0
Alex Wilmer <djazium@gmail.com>
2022-02-17 16:10:10 -0500

    ❤️

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider, Flex } from 'theme-ui'
+import cat from './cat.jpeg'
+import underline from './underline.png'
+
+export const theme = {
+  fonts: {
+    body: 'Raleway, sans-serif',
+  },
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: '#33e',
+  },
+}
+
+function Line() {
+  return <img src={underline} />
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Flex sx={{
+        flexDirection: ['column-reverse', 'row']
+      }}>
+        <Flex sx={{
+          mt: [0, '-5rem'],
+          p: '3rem',
+          flex: 1,
+          justifyContent: 'center',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}>
+
+          <Flex sx={{
+            fontStyle: 'italic',
+            fontSize: '3rem',
+          }}>Alex's 36th</Flex>
+          <Line />
+          <Flex sx={{ fontSize: '1.3rem', pb: '0.8rem' }}>Celebrating:</Flex>
+          <Flex>1 month sober</Flex>
+          <Flex>16 years of friendship</Flex>
+          <Flex>36 spins around the sun</Flex>
+          <Flex sx={{
+            p: '0.8rem'
+          }}>❤️❤️❤️</Flex>
+          <Flex sx={{ fontSize: '1.3rem', p: '0.8rem' }}>Activities:</Flex>
+          <Flex>Hang out with Glitch</Flex>
+          <Flex>Dinner & Movie</Flex>
+        </Flex>
+        <Flex sx={{
+          flex: 1
+        }}>
+          <img style={{
+            backgroundSize: 'cover',
+            width: '100%'
+          }} src={cat} />
+        </Flex>
+      </Flex>
+    </ThemeProvider>
   );
 }
 
